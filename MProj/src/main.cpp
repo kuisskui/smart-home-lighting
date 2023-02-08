8111c78c6b2543b089935468352082c460a221a9
VorakornK <vorakorn.ko@ku.th>
2023-02-08 14:31:48 +0700

    Fix

--- MProj/src/main.cpp
+++ MProj/src/main.cpp
@@ -2,6 +2,7 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <Bounce2.h>
+#include <string>
 
 #define BUTTON 27
 #define GREEN 33
@@ -13,8 +14,7 @@ String id;
 int Id;
 int Brightness;
 bool Status;
-bool Auto;
-int Ldr;
+int Auto;
 
 const char *ssid = "Qwerty";
 const char *password = "12345678";
@@ -22,21 +22,28 @@ const char *password = "12345678";
 const String UpdateUrl = "http://127.0.0.1/tap/receive/";
 const String CheckUrl = "http://127.0.0.1/tap/send/";
 
+String int_to_string(int num) {
+  String str = int_to_string(num);
+  return str;
+}
+
+int string_to_int(String str) {
+  int num = string_to_int(str);
+  return num;
+}
+
 void Connect_Wifi();
 
 void POST_Update(String id, int Brightness, bool Status, bool Auto, int Ldr);
-bool POST_Check(int ids);
+void POST_Check(int ids);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Hello world!");
   Connect_Wifi();
-  Serial.println("Hello world!");
 
 }
 
 void loop() {
-  Serial.println("Hello world!");
 }
 
 void Connect_Wifi()
@@ -54,17 +61,18 @@ void Connect_Wifi()
 }
 
 
-void POST_Update(String ids, int Brightnesss, bool Statuss, bool Autos, int Ldrs) {
+void POST_Update(int ids, bool Statuss, int Ldrs) {
     String json;
     HTTPClient http;
     http.begin(UpdateUrl);
     http.addHeader("Content-Type", "application/json");
 
     DynamicJsonDocument doc(256);
-    doc["Id"] = ids;
-    doc["Brightness"] = Brightnesss;
+    String idss = int_to_string(ids);
+    doc["Id"] = idss;
+    doc["Brightness"] = -1;
     doc["Status"] = Statuss;
-    doc["Auto"] = Autos;
+    doc["Auto"] = -1;
     doc["LDR"] = Ldrs;
     serializeJson(doc, json);
 
@@ -81,7 +89,7 @@ void POST_Update(String ids, int Brightnesss, bool Statuss, bool Autos, int Ldrs
 }
 
 
-bool POST_Check(int ids) {
+void POST_Check(int ids) {
     String json;
     HTTPClient http;
     http.begin(UpdateUrl);
@@ -96,11 +104,10 @@ bool POST_Check(int ids) {
     if (httpResponseCode == 200) {
       String payload = http.getString();
       deserializeJson(doc2, payload);
-      Id = doc2["Id"];
+      Id = string_to_int(doc2["Id"].as<String>());
       Brightness = doc2["Brightness"];
       Status = doc2["Status"];
       Auto = doc2["Auto"];
-      Ldr = doc2["LDR"];
       Serial.print("Done");
       Serial.println();
     }
