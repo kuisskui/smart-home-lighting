98e0536848e24186223e429acab76c04d50709d3
VorakornK <vorakorn.ko@ku.th>
2023-02-08 15:31:05 +0700

    Test

--- MProj/src/main.cpp
+++ MProj/src/main.cpp
@@ -35,75 +35,76 @@ void POST_Check(int ids);
 void setup() {
   Serial.begin(115200);
   Connect_Wifi();
-
+  POST_Update(2, true, 5);
+  POST_Check(2);
 }
 
 void loop() {
-  int LDRValue = map(analogRead(LDR), 0, 4095, 0, 255);
-  POST_Check(1);
-  if (Auto == false)
-  {
-    if(touchRead(1) > 20 && !peak[1]) {
-      peak[1] = true;
-      if (touch[1] == true) {
-        touch[1] = false;
-        buttonState[1] = !buttonState[1];
-      }
-    } 
-    else if(touchRead(2) < 20 && peak[1]) {
-      peak[1] = false;
-      touch[1] = true;
-    }
-    POST_Update(1, buttonState[1], LDRValue);
-  }
+  // int LDRValue = map(analogRead(LDR), 0, 4095, 0, 255);
+  // POST_Check(1);
+  // if (Auto == false)
+  // {
+  //   if(touchRead(1) > 20 && !peak[1]) {
+  //     peak[1] = true;
+  //     if (touch[1] == true) {
+  //       touch[1] = false;
+  //       buttonState[1] = !buttonState[1];
+  //     }
+  //   } 
+  //   else if(touchRead(2) < 20 && peak[1]) {
+  //     peak[1] = false;
+  //     touch[1] = true;
+  //   }
+  //   POST_Update(1, buttonState[1], LDRValue);
+  // }
 
-  if (Status)
-  {
-    ledcWrite(0, Brightness);
-  }
+  // if (Status)
+  // {
+  //   ledcWrite(0, Brightness);
+  // }
 
-  POST_Check(2);
-  if (Auto == false)
-  {
-    if(touchRead(2) > 20 && !peak[2]) {
-      peak[2] = true;
-      if (touch[2] == true) {
-        touch[2] = false;
-        buttonState[2] = !buttonState[2];
-      }
-    } 
-    else if(touchRead(15) < 20 && peak[2]) {
-      peak[2] = false;
-      touch[2] = true;
-    }
-    POST_Update(2, buttonState[2], LDRValue);
-  }
-  POST_Update(2, Status, LDRValue);
-  if (Status)
-  {
-    ledcWrite(1, Brightness);
-  }
+  // POST_Check(2);
+  // if (Auto == false)
+  // {
+  //   if(touchRead(2) > 20 && !peak[2]) {
+  //     peak[2] = true;
+  //     if (touch[2] == true) {
+  //       touch[2] = false;
+  //       buttonState[2] = !buttonState[2];
+  //     }
+  //   } 
+  //   else if(touchRead(15) < 20 && peak[2]) {
+  //     peak[2] = false;
+  //     touch[2] = true;
+  //   }
+  //   POST_Update(2, buttonState[2], LDRValue);
+  // }
+  // POST_Update(2, Status, LDRValue);
+  // if (Status)
+  // {
+  //   ledcWrite(1, Brightness);
+  // }
 
-  POST_Check(3);
-  if (Auto == false)
-  {
-    if(touchRead(3) > 20 && !peak[3]) {
-      peak[3] = true;
-      if (touch[3] == true) {
-        touch[3] = false;
-        buttonState[3] = !buttonState[3];
-      }
-    } 
-    else if(touchRead(4) < 20 && peak[3]) {
-      peak[3] = false;
-      touch[3] = true;
-    }
-  }
-  POST_Update(3, buttonState[3], LDRValue);
-  if (Status)
-  {
-    ledcWrite(2, Brightness);
-  }
+  // POST_Check(3);
+  // if (Auto == false)
+  // {
+  //   if(touchRead(3) > 20 && !peak[3]) {
+  //     peak[3] = true;
+  //     if (touch[3] == true) {
+  //       touch[3] = false;
+  //       buttonState[3] = !buttonState[3];
+  //     }
+  //   } 
+  //   else if(touchRead(4) < 20 && peak[3]) {
+  //     peak[3] = false;
+  //     touch[3] = true;
+  //   }
+  // }
+  // POST_Update(3, buttonState[3], LDRValue);
+  // if (Status)
+  // {
+  //   ledcWrite(2, Brightness);
+  // }
 }
 
 void Connect_Wifi()
@@ -128,11 +129,11 @@ void POST_Update(int ids, bool Statuss, int Ldrs) {
     http.addHeader("Content-Type", "application/json");
     http.setTimeout(10000);
     DynamicJsonDocument doc(256);
-    doc["Id"] = ids;
-    doc["Brightness"] = -1;
-    doc["Status"] = Statuss;
-    doc["Auto"] = -1;
-    doc["LDR"] = Ldrs;
+    doc["id"] = ids;
+    doc["brightness"] = -1;
+    doc["status"] = Statuss;
+    doc["auto"] = -1;
+    doc["ldr"] = Ldrs;
     serializeJson(doc, json);
 
     int httpResponseCode = http.POST(json);
@@ -157,17 +158,17 @@ void POST_Check(int ids) {
 
     DynamicJsonDocument doc(256);
     DynamicJsonDocument doc2(256);
-    doc["Id"] = ids;
+    doc["id"] = ids;
     serializeJson(doc, json);
 
     int httpResponseCode = http.POST(json);
     if (httpResponseCode == 200) {
       String payload = http.getString();
       deserializeJson(doc2, payload);
-      Id = doc2["Id"].as<int>();
-      Brightness = doc2["Brightness"].as<int>();
-      Status = doc2["Status"].as<bool>();
-      Auto = doc2["Auto"].as<int>();
+      Id = doc2["id"].as<int>();
+      Brightness = doc2["brightness"].as<int>();
+      Status = doc2["status"].as<bool>();
+      Auto = doc2["auto"].as<int>();
       Serial.print("Done");
       Serial.println();
     }
