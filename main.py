5025573e368c1faa78dd856bbd9c7cd675445b87
Kaopong Sangkittiyakorn <kuisskui@Kaopongs-MacBook-Pro.local>
2023-02-08 11:23:45 +0700

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Body
+import uvicorn
+from typing import Union, Optional, List
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+
+FORMAT_DATETIME = "%Y/%m/%d %H:%M"
+
+
+class Light(BaseModel):
+    id: Union[int, str]
+    status: bool   
+    auto: bool
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"msg": "pick a locker"}
+
+
+@app.get("/tap/")
+def send(light: Light):
+    return {"light": light}
+    
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
